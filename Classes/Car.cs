96d039e399c8f618a8fc4790ccc5b875911e5ebc
StruggsJ <strjac5@gmail.com>
2023-09-05 14:08:32 -0500

    initial commit.

---
+++ Classes/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Car
+    {
+        public Car()
+        {
+
+        }
+
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public string Make { get; set; } = "Jeep";
+        public string Model { get; set; } = "Compass";
+        public int Year { get; set; } = 2012;
+
+
+
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car dreamCar = new Car("Hyundai", "Accent", 2018);
+            Car defaultCar = new Car();
+
+            Console.WriteLine($"One of my dream cars is a {dreamCar.Year} {dreamCar.Make} {dreamCar.Model}.");
+            Console.WriteLine($"However, I remember being in a {defaultCar.Year} {defaultCar.Make} {defaultCar.Model} when I was younger.");
         }
     }
 }
